9307f8cb7e8099f6ec66bb516ccc66e3b61215d2
TerrordactylDesigns <mbelardo1@gmail.com>
2013-12-05 00:24:31 -0500

    starting the port. djAdvance event doesnt seem to work anymore.

--- index.js
+++ index.js
@@ -1,33 +1,71 @@
 //
 // Lets just proof of concept this shall we?
 //
-var fs          = require('fs')
-  , config      = loadConfig()
-  , AUTH        = config.botinfo.auth + '=?_expires=' + config.botinfo.expires + '==&user_id=' + config.botinfo.userid + '=&v=STIKLg=='
-  , ROOM        = config.room
-  , PlugAPI     = require('plugapi')
-  , UPDATECODE  = 'fe940c'
-  , bot         = new PlugAPI(AUTH, UPDATECODE)
+// var fs          = require('fs')
+//   , config      = loadConfig()
+//   , AUTH        = config.botinfo.auth + '=?_expires=' + config.botinfo.expires + '==&user_id=' + config.botinfo.userid + '=&v=STIKLg=='
+//   , ROOM        = config.room
+//   , PlugAPI     = require('plugapi')
+//   , UPDATECODE  = 'fe940c'
+//   , bot         = new PlugAPI(AUTH, UPDATECODE)
   
 
-bot.connect(ROOM)
-bot.on('roomJoin', function() {
-  console.log('joined the room')
-  bot.chat('EVERYBODY CALM THE FUCK DOWN, I\'M HERE!')
-})
+// bot.connect(ROOM)
+// bot.on('roomJoin', function() {
+//   console.log('joined the room')
+//   bot.chat('EVERYBODY CALM THE FUCK DOWN, I\'M HERE!')
+// })
 
-// Log chat
-bot.on('chat', function(data) {
-  console.log(data)
-  console.log('['+data.from+']: ' + data.message)
-})
+// // Log chat
+// bot.on('chat', function(data) {
+//   //console.log(data)
+//   console.log('['+data.from+']: ' + data.message)
+// })
 
-// Can we auto woot?
-bot.on('djAdvance', function(data) {
-  console.log(data)
-  bot.woot()
-})
+// // Can we auto woot?
+// bot.on('djAdvance', function(data) {
+//   console.log(data)
+//   bot.woot()
+// })
 
-function loadConfig() {
-  return JSON.parse(fs.readFileSync('config.json', 'ascii'))
+// function loadConfig() {
+//   return JSON.parse(fs.readFileSync('config.json', 'ascii'))
+// }
+
+/**/// Public: loadBot
+/**///
+/**/// Args
+/**/// config - parsed config
+/**///
+/**/// Returns
+/**/// bot    - a freshly built shiny new robot
+/**///
+/**/// Notes
+/**/// note   - Duct tapes together a boombot
+exports.loadBot = function(config) {
+  var robot = require('./lib/robot')
+  bot       = robot.launch(
+    config.botinfo.auth + '=?_expires=' + config.botinfo.expires + '==&user_id=' + config.botinfo.userid + '=&v=STIKLg==',
+    config.updatecode
+  )
+  return bot
+}
+/**/// Public: run
+/**///
+/**/// Args
+/**/// bot        - boombot instance
+/**/// events     - event handlers
+/**/// commands   - array of bot commands
+/**/// config     - parsed config.json
+/**/// blacklist  - array of blacklist objects
+/**/// version    - bot version
+/**///
+/**/// Returns
+/**/// return     - a fully operational battle station
+/**///
+/**/// Notes
+/**/// note       - tells the robot to listen and respond
+exports.run = function(bot, events, commands, config, version) {
+  var robot = require('./lib/robot')
+  return robot.run(bot, events, commands, config, version)
 }
---
+++ models/robot.js
@@ -0,0 +1,32 @@
+var Scribble = require('scribble')
+/**/// Public: Robot
+/**///
+/**/// Args
+/**/// boombot  - a ttapi bot instance
+/**/// events   - event router file
+/**/// commands - array of commands
+/**/// config   - parsed config.json object
+/**///
+/**/// Returns
+/**/// return   - a robot class linked to events, config, commands, and has
+/**///            variables set to defaults
+var Robot = function(boombot, events, commands, config, version) {
+  this.bot          = boombot
+  this.config       = config
+  this.events       = events
+  this.commands     = commands
+  //this.modCommands  = require('../lib/core/admin_controls')
+  this.theUsersList = {}
+  this.shutUp       = false
+  this.snagCounter  = 0
+  this.DJMode       = false
+  this.yank         = false
+  this.autoNod      = false
+  this.nextUp       = {}
+  this.version      = version
+  this.scrobble     = null
+  this.startTime    = Math.round(+new Date() / 1000)
+  //this.scribble     = (this.config.lastfm.use) ? new Scribble(this.config.lastfm.API_key, this.config.lastfm.secret, this.config.lastfm.username, this.config.lastfm.password) : null
+}
+
+module.exports = Robot
